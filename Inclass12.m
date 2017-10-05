6f1da095bf18721acf2553117e2959a7e8e7b775
noelpei0461 <31324311+noelpei0461@users.noreply.github.com>
2017-10-06 04:24:54 +0800

    Update Inclass12.m

--- Inclass12.m
+++ Inclass12.m
@@ -10,7 +10,12 @@ sigma=3;
 fgauss=fspecial('gaussian',rad,sigma);
 
 imgsmooth=imfilter(img1,fgauss);
-imshow(imgsmooth,[500,5000])
+imshow(imgsmooth,[500,5000]);
+
+imgbg=imopen(imgsmooth,strel('disk',100));
+imgsm_bg=imsubtract(imgsmooth,imgbg);
+
+imshow(imgsm_bg,[0,4500])
 % 2. threshold this image to get a mask that marks the cell nuclei. 
 img_m=img1 > 800;
 img_m=img1 > 900;
@@ -18,6 +23,8 @@ img_m=img1 > 1000;
 img_m=img1 > 1100;
 % 3. Use any morphological operations you like to improve this mask (i.e.
 % no holes in nuclei, no tiny fragments etc.)
+img_close=imclose(imgsm_bg,strel('disk',5));
+
 
 % 4. Use the mask together with the images to find the mean intensity for
 % each cell nucleus in each of the two channels. Make a plot where each data point 
