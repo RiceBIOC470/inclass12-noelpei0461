c44f0fadc627839e5cdb6c35fea16a9d610de87d
noelpei0461 <31324311+noelpei0461@users.noreply.github.com>
2017-10-13 00:14:53 +0800

    Update Inclass12.m

--- Inclass12.m
+++ Inclass12.m
@@ -16,7 +16,7 @@ imgsm_bg=imsubtract(imgsmooth,imgbg);
 
 imshow(imgsm_bg,[0,4500])
 % 2. threshold this image to get a mask that marks the cell nuclei. 
-img_m=imgsm_bg > 50;
+img_m=imgsm_bg > 60;
 img_sm1=uint16(img_m)*500;
 imshow(img_sm1,[0,800])
 % 3. Use any morphological operations you like to improve this mask (i.e.
@@ -27,3 +27,14 @@ img_close=imclose(imgsm_bg,strel('disk',5));
 % 4. Use the mask together with the images to find the mean intensity for
 % each cell nucleus in each of the two channels. Make a plot where each data point 
 % represents one nucleus and these two values are plotted against each other
+chan=1;
+iplane=reader.getIndex(zplane-2,chan-1,time-1)+1;
+img2=beGetPlane(reader,iplane);
+
+prop1=regionprops(imgsmooth,imgbg,'MeanIntensity','MaxIntensity','Area','Centroid');
+prop2=regionprops(imgsmooth,img2,'MeanIntensity','MaxIntensity','Area','Centroid');
+
+int1=[prop1.MeanIntensity];
+int2=[prop2.MeanIntensity];
+
+plot(int1,int2,'r','MarkerSize',18)
